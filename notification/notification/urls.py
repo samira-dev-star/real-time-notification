13cca358c9111353d6618c146c5f1189184896f8
sammashtizadeh@gmail.com <sammashtizadeh@gmail.com>
2025-08-08 18:28:00 +0330

    Add URL pattern for admin event stream notifications

--- notification/notification/urls.py
+++ notification/notification/urls.py
@@ -19,7 +19,22 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.contrib.auth.decorators import login_required
+import django_eventstream
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # این مسیر فقط برای ادمین است تا به رویدادها گوش دهد
+    path('admin-events/', include(django_eventstream.urls), {
+        'channels': ['admin-notifications'] # نام کانال اختصاصی ادمین
+    }),
+    
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
+
